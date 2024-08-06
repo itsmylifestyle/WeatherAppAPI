0c7533b7d3c8ba148c11cbfe9d6a0dd332afdad4
itsmylifestyle <hahaii00@mail.ru>
2024-08-06 18:39:29 +0500

    Initial Commit

---
+++ WeatherAppAPI/WeatherAppAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppAPIApp.swift
+//  WeatherAppAPI
+//
+//  Created by Айбек on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
